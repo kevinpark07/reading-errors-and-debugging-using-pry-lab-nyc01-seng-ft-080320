16d78ada936ce8e16a7da4a08d7e7a127ae8412c
Kevin Park <kevin.park07@yahoo.com>
2020-07-04 18:31:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,8 @@
 # don't forget to add: require 'pry'
-require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    binding.pry
     "ssssssssss" + string
   else
-    binding.pry
     string
   end
 end
